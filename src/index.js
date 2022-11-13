28a893a79239786079e290a2dd88119759aa8612
hayley.mcc@gmail.com <hayley.mcc@gmail.com>
2022-11-13 18:38:14 +0000

    Frontend deployment ready

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './index.css';
-import App from './App';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
 // Redux
 import { Provider } from 'react-redux';
 import { store } from './app/store';
+
+// Styles
+import './index.css';
+
+// Components
+import App from './App';
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
